bc5171371fb9da1f3cbb3f52bd4311f805b3eeb7
Abhishek Sharma <abhishek611sharma@gmail.com>
2021-12-30 12:31:07 +0530

    added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
 import { LoginComponent } from './components/customer/login/login.component';
 import { MenuComponent } from './components/customer/menu/menu.component';
 import { OrderComponent } from './components/customer/order/order.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
     LoginComponent,
     MenuComponent,
-    OrderComponent
+    OrderComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/customer/login/login.component.ts
+++ src/app/components/customer/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  registerForm:any =  FormGroup;
+  submitted = false;
+
+  //Add user form actions
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+    
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+    //True if all the fields are filled
+    if(this.submitted)
+    {
+      alert("Great!!");
+    }
+  
+  }
+
 
   ngOnInit(): void {
+    //login form
+   this.registerForm = this.formBuilder.group({
+    mobile: ['', [Validators.required,  Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+    password: ['', [Validators.required]],
+    });
   }
 
 }
