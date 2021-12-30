22cfdd7a35b6024cb5db1ffc8e2581e21c6fb011
Abhishek Sharma <abhishek611sharma@gmail.com>
2021-12-30 10:46:54 +0530

    added project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { LoginComponent } from './components/customer/login/login.component';
+import { MenuComponent } from './components/customer/menu/menu.component';
+import { OrderComponent } from './components/customer/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    LoginComponent,
+    MenuComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
